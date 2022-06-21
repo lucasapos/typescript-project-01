e38a72e4828fab42cebb1ac2c0c8dd10ac3616c6
Lucas Oliveira <aplucasoliveira@gmail.com>
2022-06-21 10:35:51 -0300

    Modulo 03 :: Pegando valores dos inputs e controlando submit do formulário

--- app/app.ts
+++ app/app.ts
@@ -1,3 +1,8 @@
-import { Negociacao } from './models/negociacao.js';
+import { negociacaoController } from "./controllers/negociacao-controller.js";
 
-const negociacao = new Negociacao(new Date(), 10, 100);
+const controller = new negociacaoController();
+const form = document.querySelector('.form');
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    controller.adiciona();
+})
